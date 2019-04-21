dea77e9577ea54cf8be8bf860718fbc993d9ddeb
GavinHe322 <44640635+GavinHe322@users.noreply.github.com>
2019-04-22 00:42:10 +0800

    Add files via upload

--- python/18_Django.py
+++ python/18_Django.py
@@ -9,7 +9,7 @@
     ORM
     模板系统
 '''
-'''
+'''2.
     特点：
         1.集成度比较高、功能比较强大
             内置了完善的路由系统、模板系统、后台数据管理功能。。。
@@ -18,4 +18,30 @@
 '''
 # pip install -i https://pypi.douban.com/simple django
 # pip install django=2.0
-# django-admin startproject 'name!'
+# django-admin startproject 'name!' #创建项目
+# py manage.py runserver  #运行
+
+'''
+    完成一个基于Django的工程的创建和运行
+    使用路由系统
+    基于：project 和 app
+    一个project可以包含多个app
+'''
+
+# py manage.py startapp 创建一个
+
+# 3. 使用路由系统
+# 路由：建立起一种url和视图之前的印设关系
+
+
+# 4. ORM
+'''
+    Object Relational Mapping -> 对象关系映射
+    不用写sql，按照对象的操作，就可以实现数据库的增删改查
+    使用模型类来完成数据的CRUD
+    使用后台管理系统直接以可视化的方式操作数据
+    使用模型类 需要在项目/settings 配置app
+    py manage.py makemigrations 迁移  模型类的激活
+    py manage.py migrate 才能生效
+'''
+# 5 
