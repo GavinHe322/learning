07852bca62772ea396d7f1000ad157e824b22d96
GavinHe322 <https://github.com/GavinHe322>
2019-05-26 23:52:51 +0800

    加载css/img/json/字体等

---
+++ webpack/README.md
@@ -0,0 +1,3 @@
+## 安装依赖 npm install
+
+## 打包 npm run build  配置在webpack.json 中 script 
--- webpack/demo01/src/index.js
+++ webpack/demo01/src/index.js
@@ -1,11 +1,20 @@
 import _ from 'lodash';
+import './style.css'
+import Icon from './icon.png'
+import Data from './data.xml';
 
 function component() {
     const element = document.createElement('div');
   
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
-  
+    element.classList.add('hello')
+
+    // add this image to our existing div
+    const myIcon = new Image();
+    myIcon.src = Icon
+    element.appendChild(myIcon);
+    console.log(Data)
     return element;
 }
   
