f863c90a6f95a599260cfce087da80c2a8f92bf5
yh <122733674@qq.com>
2019-05-27 12:55:09 +0800

    '新安装clear-plugin/html-plugin/插件'

--- webpack/demo01/src/index.js
+++ webpack/demo01/src/index.js
@@ -1,20 +1,18 @@
 import _ from 'lodash';
-import './style.css'
-import Icon from './icon.png'
-import Data from './data.xml';
+import printMe from './print.js';
 
 function component() {
     const element = document.createElement('div');
-  
+    const btn = document.createElement('button');
     // Lodash, currently included via a script, is required for this line to work
     element.innerHTML = _.join(['Hello', 'webpack'], ' ');
     element.classList.add('hello')
 
-    // add this image to our existing div
-    const myIcon = new Image();
-    myIcon.src = Icon
-    element.appendChild(myIcon);
-    console.log(Data)
+    btn.innerHTML = 'Click me and check the console!';
+    btn.onclick = printMe;
+
+    element.appendChild(btn);
+
     return element;
 }
   
--- webpack/demo01/webpack.config.js
+++ webpack/demo01/webpack.config.js
@@ -1,44 +1,21 @@
 const path = require('path')
-
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ClearWebpackPlugin = require('clean-webpack-plugin')
 module.exports = {
-    entry: './src/index.js',
+    // entry: './src/index.js',
+    entry: {
+        app: './src/index.js',
+        print: './src/print.js',
+    },
+    plugins: [
+        new ClearWebpackPlugin(),
+        new HtmlWebpackPlugin({
+            title: 'Output management/webpack.config.js设置的title?',
+        })
+    ],
     output: {
-        filename: 'build.js',
+        // filename: 'build.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist')
     },
-    module: {
-        rules: [
-            {
-                test: /\.css$/,
-                use: [
-                    'style-loader',
-                    'css-loader'
-                ]
-           },
-           {
-               test: /\.(png|svg|jpg|gif)$/,
-               use: [
-                   'file-loader'
-               ]
-           },
-           {
-                test: /\.(woff|woff2|eot|ttf|otf)$/,
-                use: [
-                   'file-loader'
-                ]
-            },
-            {
-                test: /\.(csv|tsv)$/,
-                use: [
-                    'csv-loader'
-                ]
-            },
-            {
-                test: /\.xml$/,
-                use: [
-                    'xml-loader'
-                ]
-            }
-        ]
-    }
 };
