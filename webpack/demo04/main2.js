f1ec1e80409e06ee5229152d95d748f0a1513dcd
GavinHe322 <https://github.com/GavinHe322>
2019-06-02 20:26:08 +0800

    导出模块,打包成两个js

---
+++ webpack/demo04/main2.js
@@ -0,0 +1,5 @@
+import main1 from './main1'
+
+main1()
+document.write('<br/>')
+document.write('hello this is main2.js')
---
+++ webpack/demo04/webpack.config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    entry: {
+        build1: './main1.js',
+        build2: './main2.js',
+    },
+    output: {
+        filename: '[name].js'
+    }
+}
