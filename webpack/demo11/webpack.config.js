9e89f4ef38f1c8445aebf29e20317d075fc2ad17
yh <122733674@qq.com>
2019-06-14 17:01:53 +0800

    '地图'

---
+++ webpack/demo11/webpack.config.js
@@ -0,0 +1,16 @@
+var webpack = require('webpack')
+
+var devFlagPlugin = new webpack.DefinePlugin({
+    __DEV__: JSON.stringify(JSON.parse(process.env.DEBUG || "false" ))
+})
+
+
+module.exports = {
+    entry: './main.js',
+    output: {
+        filename: 'build.js'
+    },
+    plugins: [
+        devFlagPlugin
+    ]
+}
