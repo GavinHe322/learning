20dea3f0cf0bc71061381df02fc3966b66c58e2a
yh <122733674@qq.com>
2019-06-15 12:03:05 +0800

    '无重复'

--- algorithm/code/03.js
+++ algorithm/code/03.js
@@ -1,3 +1,20 @@
-function square () {
-    
+function square (str) {
+    var obj = {}
+    for (var el of str) {
+        if ( Object.keys(obj).indexOf(el) == -1 ) {
+            obj[el] = el
+        }
+    }
+    return Object.keys(obj).length
 }
+
+var str = 'abcabcbb'
+console.log(
+    square(str)
+)
+
+var repeatStr = 'bbbb'
+
+console.log(
+    square(repeatStr)
+)
