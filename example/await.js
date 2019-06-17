6e37e1e936c52cba3987a09c7c738108cad301cf
GavinHe322 <https://github.com/GavinHe322>
2019-06-17 19:16:03 +0800

    await

---
+++ example/await.js
@@ -0,0 +1,12 @@
+    const p = Promise.resolve();
+
+    (async () => {
+    await p;
+    console.log("after:await");
+    })();
+
+    p.then(() => {
+    console.log("tick:a");
+    }).then(() => {
+    console.log("tick:b");
+    });
