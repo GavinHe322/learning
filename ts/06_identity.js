6058a933963dda8e3095ce967b4ec72ba70f0ed8
GavinHe322 <https://github.com/GavinHe322>
2019-06-27 19:12:32 +0800

    拉去分支

--- ts/06_identity.js
+++ ts/06_identity.js
@@ -8,6 +8,14 @@
  * 一个组件可以支持多种类型的数据。 这样用户就可以以自己的数据类型来使用组件。
  */
 //   主要解决 类、接口、方法的复用性。以及对不确定数据类型的支持
+/**
+ *
+ * @param value
+ * 泛型的定义
+ * 泛型函数
+ * 泛型类
+ * 泛型接口
+ */
 // 只能返回 string 类型的数据
 function getData(value) {
     return value;
@@ -21,4 +29,81 @@ function getData3(value) {
     return value;
 }
 console.log(getData3('泛型'));
-// 同时返回 任何类型
+// 泛型类 比如有个最小堆算法， 需要通知支持str num 两种类型， 通过泛型实现
+var MinClass = /** @class */ (function () {
+    function MinClass() {
+        this.list = [];
+    }
+    MinClass.prototype.add = function (num) {
+        this.list.push(num);
+    };
+    MinClass.prototype.min = function () {
+        return Math.min.apply(this, this.list);
+    };
+    return MinClass;
+}());
+var min = new MinClass();
+min.add(11);
+min.add(12);
+min.add(13);
+min.add(10);
+console.log(min.min());
+var MinClass2 = /** @class */ (function () {
+    function MinClass2() {
+        this.list = [];
+    }
+    MinClass2.prototype.add = function (value) {
+        this.list.push(value);
+    };
+    MinClass2.prototype.min = function () {
+        return Math.min.apply(this, this.list);
+    };
+    return MinClass2;
+}());
+var min2 = new MinClass2();
+var min3 = new MinClass2();
+min2.add(2);
+min2.add(32);
+min2.add(0);
+min3.add('b');
+min3.add('a');
+min3.add('c');
+console.log(min2.min(), min3.min());
+var fn1 = function (value1, value2) {
+    return value1 + value2;
+};
+console.log(fn1('账号', '很好'));
+var fn2 = function (string) {
+    return string;
+};
+console.log(fn2('string fn2'));
+function getData4(value) {
+    return value;
+}
+var fn3 = getData4;
+console.log(fn3('fn3 通过赋值 函数 输出'));
+var MysqlDB = /** @class */ (function () {
+    function MysqlDB() {
+    }
+    MysqlDB.prototype.add = function (info) {
+        console.log(info);
+        return true;
+    };
+    MysqlDB.prototype.update = function (info, id) {
+        console.log(info, id, 'update');
+        return true;
+    };
+    return MysqlDB;
+}());
+var User = /** @class */ (function () {
+    function User(parms) {
+        this.name = parms.name;
+        this.pwd = parms.pwd;
+    }
+    return User;
+}());
+var user = new User({ name: 'gavin', pwd: '124' });
+console.log(user);
+var DB = new MysqlDB();
+DB.add(user);
+DB.update(user, 1);
--- ts/06_identity.ts
+++ ts/06_identity.ts
@@ -11,6 +11,15 @@
 
 //   主要解决 类、接口、方法的复用性。以及对不确定数据类型的支持
 
+/**
+ * 
+ * @param value 
+ * 泛型的定义
+ * 泛型函数
+ * 泛型类
+ * 泛型接口
+ */
+
 
 // 只能返回 string 类型的数据
 
@@ -33,6 +42,133 @@ console.log(
 )
 
 
+// 泛型类 比如有个最小堆算法， 需要通知支持str num 两种类型， 通过泛型实现
 
-// 同时返回 任何类型
+class MinClass {
+    public list: number[] = []
+    add(num: number) {
+        this.list.push(num)
+    }
+    min() {
+        return Math.min.apply(this, this.list)
+    }
+}
 
+var min = new MinClass()
+
+min.add(11)
+min.add(12)
+min.add(13)
+min.add(10)
+
+console.log(
+    min.min()
+)
+
+class MinClass2<T> {
+    public list: T[] = [];
+    add(value: T): void {
+        this.list.push(value)
+    }
+    min():T {
+        return Math.min.apply(this, this.list)
+    }
+}
+
+var min2 = new MinClass2<number>()
+var min3 = new MinClass2<string>()
+
+min2.add(2)
+min2.add(32)
+min2.add(0)
+
+min3.add('b')
+min3.add('a')
+min3.add('c')
+
+console.log(
+    min2.min(),
+    min3.min()
+)
+
+
+// 泛型接口
+
+interface Config {
+    (value1: string, value2: string): string
+}
+
+
+var fn1: Config = function(value1: string, value2: string): string {
+        return value1 + value2
+}
+
+console.log(
+    fn1('账号', '很好')
+)
+
+// 第一种
+interface ConfigFn {
+    <T>(value:T): T
+}
+
+var fn2: ConfigFn = function<T>(string:T): T {
+    return string
+}
+console.log(
+    fn2<string>('string fn2')
+)
+
+// 第二种
+
+interface ConfigFn3 <T> {
+    (value: T): T;
+}
+
+function getData4<T> (value: T): T {
+    return value
+}
+
+
+var fn3: ConfigFn3<string> = getData4
+console.log(
+    fn3('fn3 通过赋值 函数 输出')
+)
+
+
+class MysqlDB<T> {
+    add(info:T): boolean {
+        console.log(info)
+        return true
+    }
+    update(info:T, id: number ): boolean {
+        console.log(info, id, 'update')
+        return true
+    }
+}
+
+class User {
+    name: string | undefined
+    pwd: string | undefined
+    constructor(
+        parms: {
+            name: string | undefined,
+            pwd: string | undefined
+        }
+    ) {
+        this.name = parms.name
+        this.pwd = parms.pwd
+    }
+}
+
+var user = new User(
+    {name:'gavin',pwd: '124'}
+)
+
+console.log(user)
+
+var DB = new MysqlDB<User>()
+
+DB.add(user)
+
+DB.update(user, 1)
