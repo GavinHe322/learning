9b9ffa06d09e4f961d7d5941bb562035a7d7fea0
GavinHe322 <https://github.com/GavinHe322>
2019-07-06 23:00:44 +0800

    插入排序

---
+++ algorithm/sort/2_selectSort.js
@@ -0,0 +1,40 @@
+/**
+ * 首先在未排序序列中找到最小（大）元素，存放到排序序列的起始位置，
+ * 然后，再从剩余未排序元素中继续寻找最小（大）元素，然后放到已排序序列的末尾。
+ * 以此类推，直到所有元素均排序完毕。
+ */
+
+ function selectionSort(arr) {
+     console.time('选择排序')
+     var len = arr.length
+     var minIndex, temp
+
+     for (var i = 0 ; i < len - 1; i++) {
+         minIndex = i
+         for (var j = i + 1; j < len; j++) {
+            //  1.找到比 当前 arr[i] 小的
+             if (arr[j] < arr[minIndex]) {
+                 minIndex = j
+             }
+         }
+        //  2.然后在这里交换
+         temp = arr[i]
+         arr[i] = arr[minIndex]
+         arr[minIndex] = arr[i]
+     }
+     console.timeEnd('选择排序')
+    //  return arr
+ }
+var arr = []
+ for (var i = 0; i < 1000; i ++) {
+    arr.push(
+      Math.ceil(Math.random() * 100)
+    )
+}
+
+console.log(
+    selectionSort(arr)
+)
+
+// 算法分析
+// 1. 最佳/最差/平均  T(n) = O(n2)
---
+++ algorithm/sort/3_insertionSort.js
@@ -0,0 +1,49 @@
+/**
+ * 它的工作原理是通过构建有序序列，对于未排序数据，
+ * 在已排序序列中从后向前扫描，找到相应位置并插入。
+ * 插入排序在实现上，通常采用 in-place 排序
+ * （即只需用到 O(1)的额外空间的排序），因而在从后向前扫描过程中，
+ * 需要反复把已排序元素逐步向后挪位，为最新元素提供插入空间。
+ */
+
+/**
+ * <1>.从第一个元素开始，该元素可以认为已经被排序；
+    <2>.取出下一个元素，在已经排序的元素序列中从后向前扫描；
+    <3>.如果该元素（已排序）大于新元素，将该元素移到下一位置；
+    <4>.重复步骤 3，直到找到已排序的元素小于或者等于新元素的位置；
+    <5>.将新元素插入到该位置后；
+    <6>.重复步骤 2~5。
+ */
+
+console.time('插入排序')
+ function insertionSort(arr) {
+    var len = arr.length
+    for (var i = 1; i < len; i++) {
+        debugger
+        var key = arr[i]
+        var j = i -1
+        while(j >= 0 && arr[j] > key) {
+            arr[j + 1] = arr[j]
+            j--
+        }
+        arr[j + 1] = key
+    }
+    console.timeEnd('插入排序')
+    return arr
+ }
+
+ var arr = []
+ for (var i = 0; i < 5; i ++) {
+    arr.push(
+      Math.ceil(Math.random() * 1000)
+    )
+}
+
+console.log(
+    insertionSort(arr)
+)
+/**
+   最佳情况：输入数组按升序排列。(T(n) = O(n))
+   最坏情况：输入数组按降序排列。(T(n) = O(n2))
+   平均情况：(T(n) = O(n2))
+ */
