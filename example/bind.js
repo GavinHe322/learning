dac7c7b0be8758e919e862e95c271a99121d95b8
GavinHe322 <https://github.com/GavinHe322>
2019-07-18 20:10:59 +0800

    手工 bind apply call

--- example/bind.js
+++ example/bind.js
@@ -1,34 +1,64 @@
-// the bind() mtehod creates a mew function that, when called, has its this keyword
-// set to the provided value, with a given sequence of arguments preceding
-// any provided when the new function is called.
-var module = {
-    x: 24,
-    fn: function() {
-        console.log(this.x)
-        return this.x
+// // the bind() mtehod creates a mew function that, when called, has its this keyword
+// // set to the provided value, with a given sequence of arguments preceding
+// // any provided when the new function is called.
+// var module = {
+//     x: 24,
+//     fn: function() {
+//         console.log(this.x)
+//         return this.x
+//     }
+// }
+
+// var unboundGetX = module.fn
+
+// console.log(
+//     unboundGetX()
+// )
+
+// var unboundGetX = unboundGetX.bind(module)
+// console.log(
+//     unboundGetX()
+// )
+
+// this.x = 9; // this refers to global 'window' object here in the browser
+
+// // the simples use of bind() is to make a function that, no matter how it is called,
+// // is called with a particular this value. A common mistake for new js programmers is
+// // to extract a method from an object. then to later call that function and
+// // expect it to use the original object as its this (e.g. by using that method in callback-based code).
+// // without special care. however, the original object is usually lost. Creating a bound function
+// // from the function, using the original object, neatly solves this problem.
+
+// var module = {
+//     x: 91,
+//     getX: function() {
+//         console.log(this.x)
+//         return this.x
+//     }
+// }
+
+// var retriveX = module.getX
+// retriveX()
+
+// retriveX = retriveX.bind(module)
+// retriveX()
+// // return 9 - the function gets invoked at the global scope
+Function.prototype.mybind = function (context) {
+    if (typeof this != 'function') {
+        throw new TypeError('Error')
+    }
+    let _this = this
+    let arg = [...arguments].slice(1)
+    console.log(arguments)
+    return function F() {
+        // 处理函数使用 new 的情况
+        if (this instanceof F) {
+            return new _this(...arg, ...arguments)
+        } else {
+            return _this.apply(context, arg.concat(...arguments))
+        }
     }
 }
-
-var unboundGetX = module.fn
-
-console.log(
-    unboundGetX()
-)
-
-var unboundGetX = unboundGetX.bind(module)
-console.log(
-    unboundGetX()
-)
-
-this.x = 9; // this refers to global 'window' object here in the browser
-
-// the simples use of bind() is to make a function that, no matter how it is called,
-// is called with a particular this value. A common mistake for new js programmers is
-// to extract a method from an object. then to later call that function and
-// expect it to use the original object as its this (e.g. by using that method in callback-based code).
-// without special care. however, the original object is usually lost. Creating a bound function
-// from the function, using the original object, neatly solves this problem.
-
 var module = {
     x: 91,
     getX: function() {
@@ -36,10 +66,7 @@ var module = {
         return this.x
     }
 }
-
 var retriveX = module.getX
 retriveX()
-
-retriveX = retriveX.bind(module)
-retriveX()
-// return 9 - the function gets invoked at the global scope
+retriveX = retriveX.mybind(module)
+retriveX(1)
