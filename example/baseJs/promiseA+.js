288ddcf30a1fa4b59c33a704360aa92430544b2f
yh <122733674@qq.com>
2019-08-06 09:19:44 +0800

    .

--- example/baseJs/promiseA+.js
+++ example/baseJs/promiseA+.js
@@ -359,7 +359,7 @@ function myPromise(constructor) {
     if (self.status == 'pending') {
       self.status = 'rejected'
       self.reason = reason
-      self.onRejectedArray.forEach(function f() {
+      self.onRejectedArray.forEach(function (f) {
         f(self.reason)
       })
     }
@@ -520,3 +520,107 @@ b.then(function(e) {
 // c.then(function(e) {
 //   console.log(e)
 // })
+
+function myPromise(constructor) {
+  let self = this
+  self.status = 'pending'
+  self.value = undefined // if pending -> resolved
+  self.reason = undefined // if pending -> rejected
+  self.onFullfilledArray = [] // to deal with async (resolved)
+  self.onRejectedArray = [] // to deal with async (rejected)
+
+  function resolve(value) {
+    // pending -> resolved
+    if (self.status === 'pending') {
+      self.status = 'resolved'
+      self.value = value
+      self.onFullfilledArray.forEach(function (f) {
+        f(self.value)
+      })
+    }
+
+    function reject(reason) {
+      if (self.status == 'pending') {
+        self.status = 'rejected'
+        self.reason = reason
+        self.onRejectedArray.forEach(function (f) {
+          f(self.reason)
+        })
+      }
+    }
+    // according to the definition that the function 'constructor' accept two parameters
+    // erro catch
+    try {
+      constructor(resolve, reject)
+    } catch (e) {
+      reject(e)
+    }
+  }
+}
+
+myPromise.prototype.then = function(onFullfilled, onRejected) {
+  onFullfilled = typeof onFullfilled === 'function' ? onFullfilled : function (x) {return x}
+  onRejected = typeof onRejected === 'function' ? onRejected : function(e) {throw e}
+
+  let self = this
+  let promise2
+
+  switch(self.status) {
+    case 'pending':
+      promise2 = new myPromise(function(resolve, reject) {
+        self.onFullfilledArray.push(function() {
+          setTimeout(function () {
+            try {
+              let temple = onFullfilled(self.value)
+              resolvePromise(promise2, temple, resolve, reject)
+            } catch (e) {
+              reject(e)
+            }
+          })
+        })
+      }) 
+      self.onRejectedArray.push(function() {
+        setTimeout(function() {
+          try {
+            let temple = onRejected(self.reason)
+            resolvePromise(promise2, temple, resolve, reject)
+          } catch (e) {
+            reject(e)
+          }
+        })
+      })
+      break
+    case 'resolved':
+      promise2 = new myPromise(function(resolve, reject) {
+        setTimeout(function() {
+          try {
+            let temple = onFullfilled(self.value)
+            resolvePromise(promise2, temple, resolve, reject)
+          } catch (e) {
+            reject(e)
+          }
+        })
+      })
+      break
+    default:
+  }
+}
+
+function resolvePromise(promise, x, resolve, reject) {
+  if (promise === x) {
+    return reject(new TypeError('Cyclic reference'))
+  }
+  let isUsed
+  if (x !== null && (typeof x === 'object' || typeof x === 'function')) {
+    try {
+      let then = x.then
+      if (typeof then === 'function') {
+        then.call(x, function(y) {
+          if (isUsed) return
+        })
+      }
+    } catch (e) {
+      reject(e)
+    }
+  }
+}
