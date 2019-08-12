7340904a0058ba3868ccd9832fa97854c6552951
GavinHe322 <https://github.com/GavinHe322>
2019-08-12 12:59:10 +0800

    .

--- vue/vue/src/core/instance/init.js
+++ vue/vue/src/core/instance/init.js
@@ -17,7 +17,36 @@ export function initMixin (Vue: Class<Component>) {
         // a uid
         vm._uid = uid++;
         let startTag, endTag;
-        // istanbul ignore if
+        /* istanbul ignore if */
+        if (process.env.NODE_ENV !== 'production' && config.performance && mark) {
+            startTag = `vue-perf-start:${vm._uid}`;
+            endTag = `vue-perf-end:${vm._uid}`;
+            mark(startTag);
+        }
+
+        // a flag to avoid this being observed
+        vm._isVue = true;
+        // merge options
+        if (options && options._isComponent) {
+            // optimize internal component instantiation
+            // since dynamic options merging is pretty slow, and none of the
+            // internal component options needs special treatment.
+            initInternalComponent(vm, options)
+        } else {
+            vm.$options = mergeOptions(
+                resolveConstructorOptions(vm.constructor),
+                options || {},
+                vm
+            )
+        }
+        
+        /* istanbul ignore else */
+        if (process.env.NODE_ENV !== 'production') {
+            initProxy(vm);
+        } else {
+            vm._renderProxy = vm;
+        }
+        // expose real self
         
     }
 }
