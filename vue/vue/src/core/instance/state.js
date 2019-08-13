a3653e1994992121a339a949c6d4cce9df819622
GavinHe322 <https://github.com/GavinHe322>
2019-08-13 21:37:44 +0800

    core/instance/state.js-initComputed-fun

--- vue/vue/src/core/instance/state.js
+++ vue/vue/src/core/instance/state.js
@@ -149,3 +149,45 @@ function initData (vm: Component) {
     // observe data
     observe(data, true /** asRootData */)
 }
+
+const computedWatcherOptions = { lazy: true };
+
+function initComputed (vm: Component, computed: Object) {
+    // $flow-disable-line
+    const watchers = vm._computedWatchers = Object.create(null);
+    // computed properties are just getters during SSR
+    const isSSR = isServerRendering();
+
+    for (const key in computed) {
+        const userDef = computed[key];
+        const getter = typeof userDef === 'function' ? userDef : userDef.get;
+        if (process.env.NODE_EVN !== 'production' && getter == null) {
+            warn(
+                `Getter is missing for computed property "${key}"`,
+                vm
+            )
+        }
+
+        if (!isSSR) {
+            // create internal watcher for the computed property.
+            watchers[key] = new Watcher(
+                vm,
+                getter || noop,
+                noop,
+                computedWatcherOptions
+            )
+        }
+    }
+    // component-defined computed properties are already defined on the
+    // component prototype. We only need to define computed properties defined
+    // at instantiation here.
+    if (!(key in vm)) {
+        defineComputed(vm, key, userDef);
+    }  else if (process.env.NODE_EVN !== 'production') {
+        if (key in vm.$data) {
+            warn(`Teh computed property "${key}" is already defined in data`, vm);
+        } else if (vm.$options.props && key in vm.$options.props) {
+            warn(`The computed property "${key}" is already defined as a prop.`, vm)
+        }
+    }
+}
