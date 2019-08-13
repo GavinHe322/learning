a6a14263bb32ed53d423fff8213545a509b924d7
GavinHe322 <https://github.com/GavinHe322>
2019-08-13 22:20:31 +0800

    update-core-instance-state.js-createWatcher-stateMixin

--- vue/vue/src/core/instance/state.js
+++ vue/vue/src/core/instance/state.js
@@ -287,3 +287,74 @@ function initWatch (vm: Component, watch, Object) {
         }
     }
 }
+
+
+function createWatcher (
+    vm: Component,
+    expOrFn: stirng | Fcuntion,
+    handle: any,
+    options?: Object
+) {
+    if (isPlainObject(handle)) {
+        options = handle;
+        handle = handle.handle;
+    }
+    if (typeof handle === 'string') {
+        handle = vm[handle];
+    }
+    return vm.$watch(expOrFn, handle, options);
+}
+
+
+export function stateMixin (Vue: Class<Component>) {
+    // flow somehow has problems with directly declared definition object
+    // when using Object. defineProperty, so we have to procedurally build up
+    // the object here.
+    const dataDef = {};
+    dataDef.get = function () { return this._data };
+    const propsDef = {};
+    propsDef.get = function () {return this._props };
+    if (process.env.NODE_EVN !== 'production') {
+        dataDef.set = function () {
+            warn(
+                `Avoid replacing instance root $data` +
+                'Use nested data properties instead.',
+                this
+            )
+        }
+        propsDef.set = function () {
+            warn(`$props is readonly.`, this);
+        }
+    }
+    Object.defineProperty(Vue.prototype, '$data', dataDef);
+    Object.defineProperty(Vue.prototype, '$props', propsDef);
+
+    Vue.prototype.$set = set;
+    Vue.prototype.$delete = del;
+
+    Vue.prototype.$watch = function (
+        expOrFn: string | Function,
+        cb: any,
+        options?: Object
+    ): Function {
+        const vm: Component = this;
+        if (isPlainObject(cb)) {
+            return createWatcher(vm, expOrFn, cb, options);
+        }
+
+        options = options || {};
+        options.user = true;
+        const watcher = new Watcher(vm, expOrFn, cb, options);
+        if (options.immediate) {
+            try {
+                cb.call(vm, watcher.value);
+            } catch (error) {
+                handleError(error, vm, `callback for immediate watcher "${watcher.expression}"`)
+            }
+        }
+
+        return function unwatchFn () {
+            watcher.tearDown();
+        }
+    }
+}
