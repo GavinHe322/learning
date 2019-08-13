323a3d11b88495b2b793c1085458f9e445aa68b6
GavinHe322 <https://github.com/GavinHe322>
2019-08-13 21:50:13 +0800

    core/instance/state.js-createComputedGetter-createGetterInvoker-fun

--- vue/vue/src/core/instance/state.js
+++ vue/vue/src/core/instance/state.js
@@ -224,3 +224,23 @@ export function defineComputed (
     Object.defineProperty(target, key, sharedPropertyDefinition);
 }
 
+function createComputedGetter (key) {
+    return function computedGetter () {
+        const watcher = this._computedWatchers && this._computedWatchers[key]
+        if (watcher) {
+            if (watcher.dirty) {
+                watcher.evaluate();
+            }
+            if (Dep.target) {
+                watcher.depend()
+            }
+            return watcher.value;
+        }
+    }
+}
+
+function createGetterInvoker (fn) {
+    return function computedGetter () {
+        return fn.call(this, this)
+    }
+}
