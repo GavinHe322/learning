0f0e47472294d3bc54fb77be5c02459c3092ad14
GavinHe322 <https://github.com/GavinHe322>
2019-08-13 13:53:58 +0800

    update-core/instance/state-initData-func

--- vue/vue/src/core/instance/state.js
+++ vue/vue/src/core/instance/state.js
@@ -108,4 +108,44 @@ function initProps (vm: Component, propsOptions: Object) {
     }
 }
 
-
+function initData (vm: Component) {
+    let data = vm.$options.data;
+    data = vm._data = typeof data === 'function'
+      ? getData(data, vm)
+      : data || {};
+    if (!isPlainObject(data)) {
+        data = {};
+        process.env.NODE_EVN !== 'production' && warn(
+            'data functions should return an object:\n' +
+            '官网，',
+            vm
+        )
+    }
+    // proxy data an instance
+    const keys = Object.keys(data);
+    const props = vm.$options.props;
+    const methods = vm.$options.methods;
+    let i = keys.length;
+    while (i--) {
+        const key = keys[i];
+        if (process.env.NODE_EVN !== 'production') {
+            if (methods && hasOwn(methods, key)) {
+                warn(
+                    `Method "${key}" has already been defined as a data property`,
+                    vm
+                )
+            }
+            if (props && hasOwn(props, key)) {
+                process.env.NODE_EVN !== 'production' && warn(
+                    `The data property "${key}" is already declared as a prop.` +
+                    `Use prop default value instead.`,
+                    vm
+                )
+            } else if (!isReserved(key)) {
+                proxy(vm, '_data', key);
+            }
+        }
+    }
+    // observe data
+    observe(data, true /** asRootData */)
+}
