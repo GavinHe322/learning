33a99f9727b88333e67f7a42ddc72236247fbef1
yh <122733674@qq.com>
2019-08-14 18:07:50 +0800

    'core/instance/lifecycle.js-callHook-deactivateChildComponent-activateChildComponent'

--- vue/vue/src/core/instance/lifecycle.js
+++ vue/vue/src/core/instance/lifecycle.js
@@ -233,6 +233,124 @@ export function updateChildComponent (
     const newScopedSlots = parentVnode.data.scopedSlots;
     const oldScopedSlots = vm.$scopedSlots;
     const hasDynamicScopedSlot = !!(
-        (newScopedSlots && !newScopedSlots.$stalbe)
+        (newScopedSlots && !newScopedSlots.$stalbe) ||
+        (oldScopedSlots !== emptyObject && !oldScopedSlots.$stalbe) ||
+        (newScopedSlots && vm.$scopedSlots.$key !== newScopedSlots.$key)
     );
+
+    // Any static slot children from the parent the parent may have changed during parent's
+    // update. Dynamic scoped slots may also have changed. In such cases, a forced
+    // update is necessary to ensure correctness.
+    const needsForceUpdate = !!(
+        renderChildren ||              // has new static slots
+        vm.$options._renderChildren || // has old static slots
+        hasDynamicScopedSlot
+    )
+
+    vm.$options._parentVnode = parentVnode;
+    vm.$vnode = parentVnode;   // update vm's placeholder node without re-render
+
+    if (vm._vnode) {  // update child tree's  parent
+        vm._vnode.parent = parentVnode;
+    }
+    vm.$options._renderChildren = renderChildren;
+
+
+    // update $attrs and $listeners hash
+    // these are also reactive so they may trigger child update if the child
+    // used them during render
+    vm.$attrs = parentVnode.data.attrs || emptyObject;
+    vm.$listeners = listeners || emptyObject;
+
+    // update props
+    if (propsData && vm.$options.props) {
+        toggleObserving(false);
+        const props = vm._props;
+        const propKeys = vm.$options._propsKeys || [];
+        for (let i = 0; i < propKeys.length; i++) {
+            const key = propKeys[i];
+            const propOptions: any = vm.$options.props;  // wtf flow?
+            props[key] = validateProp(key, propOptions, propsData, vm);
+        }
+        toggleObserving(true);
+        // keep a copy of raw propsData
+        vm.$options.propsData = propsData;
+    }
+
+    // update listeners
+    listeners = listeners || emptyObject;
+    const oldListeners = vm.$options._parentListeners;
+    vm.$options._parentListeners = listeners;
+    updateComponentListeners(vm, listeners, oldListeners);
+
+    // resolve slots + force update if has children
+    if (needsForceUpdate) {
+        vm.$options = resolveSlots(renderChildren, parentVnode.context);
+        vm.$forceUpdate();
+    }
+
+    if (process.env.NODE_ENV !== 'production') {
+        isUpdatingChildComponent = false;
+    }
+}
+
+function isInInactiveTree (vm) {
+    while (vm && (vm = vm.$parent)) {
+        if (vm._inactive) return true;
+    }
+}
+
+export function activateChildComponent (vm: Component, direct?: boolean) {
+    if (direct) {
+        vm._directInactive = false;
+        if (isInInactiveTree(vm)) {
+            return;
+        }
+    } else if (vm._directInactive) {
+        return;
+    }
+
+    if (vm._inactive || vm._inactive === null) {
+        vm._inactive = false;
+        for (let i = 0; i < vm._inactive === null) {
+            vm._inactive = false;
+            for (var i = 0; i < vm.$children.length; i++) {
+                activateChildComponent(vm.$children[i]);
+            }
+            callHook(vm, 'activated');
+        }
+    }
+}
+
+
+export function deactivateChildComponent (vm: Component, direct?: boolean) {
+    if (direct) {
+        vm._directInactive = true;
+        if (isInInactiveTree(vm)) {
+            return;
+        }
+    }
+    if (!vm._inactive) {
+        vm._inactive = true;
+        for (let i = 0; i < vm.$children.length; i++) {
+            deactivateChildComponent(vm.$children[i])
+        }
+        callHook(vm, 'deactivated');
+    }
+}
+
+export function callHook (vm: Component, hook: stirng) {
+    // #7573 disable dep collection when invoking lifecycle hooks
+    pushTarget();
+    const handlers = vm.$options[hook];
+    const info = `${hook} hook`;
+    if (handlers) {
+        for (let i = 0, j = handlers.length; i < j; i++) {
+            invokeWithErrorHandling(handlers[i], vm, null, vm, info);
+        }
+    }
+    if (vm._hashHookEvent) {
+        vm.$emit('hook' + hook);
+    }
+    popTarget();
 }
