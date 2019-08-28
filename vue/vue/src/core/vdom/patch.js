0221260b8136b2704a3a931df7fde442ec778bad
hgx <1521550385@qq.com>
2019-08-28 14:26:00 +0800

    'update-core/vdom/patch.js-sameVnode-sameInputType-func'

--- vue/vue/src/core/vdom/patch.js
+++ vue/vue/src/core/vdom/patch.js
@@ -1,3 +1,61 @@
 /**
  * Virtual Dom patching algorithm based on Snabbdom by 
+ * Simon Firis Vindum (@paldepind)
+ * Licensed under the MIT License
+ * https://github.com
+ * 
+ * modified by Evan You (@yyx990803);
+ * 
+ * Not type-checking this because this file is perf-critical and the cost
+ * of making flow understand it is not worth it.
  */
+
+ import VNode, { cloneVNode } from './vnode';
+ import config from '../config';
+ import { SSR_ATTR } from 'shared/constants';
+ import { registerRef } from '../observer/traverse';
+ import { traverse } from '../observer/traverse';
+ import { activeInstance } from '../instance/lifecycle';
+ import { isTextInputTpye } from 'web/util/element';
+
+ import {
+     warn,
+     isDef,
+     isUndef,
+     isTrue,
+     makeMap,
+     isRegExp,
+     isPrmitive
+ } from '../util/index';
+
+ export const emptyNode = new VNode('', {} ,[]);
+
+ const hooks = ['create', 'activate', 'update', 'remove', 'destroy'];
+
+ function sameVnode (a, b) {
+     return (
+         a.key === b.key && (
+             (
+                 a.tag === b.tag &&
+                 a.isComment === b.isComment &&
+                 isDef(a.data) === isDef(b.data) &&
+                 sameInputType(a, b)
+             ) || (
+                 isTrue(a.isAsyncPlaceholder) &&
+                 a.asyncFactory === b.asyncFactory &&
+                 isUndef(b.asyncFactory.error)
+             )
+         )
+     )
+ }
+
+ function sameInputType (a, b) {
+     if (a.tag !== 'input') return true;
+     let i;
+     const tpyeA = isDef(i = a.data) && isDef(i = i.attrs) && i.type;
+     const typeB = isDef(i = b.data) && isDef(i = i.attrs) && i.type;
+     return tpyeA == typeB || isTextInputTpye(tpyeA) && isTextInputTpye(typeB);
+ }
+
+
+ 
