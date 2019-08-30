a41f9f9b8467cdaf05d426dbe78859e89b29a5e0
hgx <1521550385@qq.com>
2019-08-30 13:24:41 +0800

    'update-core/vdom/patch.js-patchVnode-findIdxInOld-checkDuplicateKeys-func'

--- vue/vue/src/core/vdom/patch.js
+++ vue/vue/src/core/vdom/patch.js
@@ -463,7 +463,118 @@ function invokeCreateHooks (vnode, insertedVnodeQueue) {
             }
         }
     }
-    
+
+    function checkDuplicateKeys (children) {
+        const seenKeys = {};
+        for (let i = 0; i < children.length; i++) {
+            const vnode = children[i];
+            const key = vnode.key;
+            if (isDef(key)) {
+                if (seenKeys[key]) {
+                    warn(
+                        `Duplicate keys detected: '${key}'. This may cause an update error.`,
+                        vnode.context
+                    );
+                } else {
+                    seenKeys[key] = true;
+                }
+            }
+        }
+    }
+
+    function findIdxInOld (node, oldCh, start, end) {
+        for (let i = start; i < end; i++) {
+            const c = oldCh[i];
+            if (isDef(c) && sameVnode(node, c)) return i;
+        }
+    }
+
+    function patchVnode (
+        oldVnode,
+        vnode,
+        insertedVnodeQueue,
+        ownerArray,
+        index,
+        removeOnly
+    ) {
+        if (oldVnode == vnode) {
+            return;
+        }
+
+        if (isDef(vnode.elm) && isDef(ownerArray)) {
+            // clone reused vnode
+            vnode = ownerArray[index] = cloneVNode(vnode);
+        }
+
+        const elm = vnode.elm = oldVnode.elm;
+
+        if (isTrue(oldVnode.isAsyncPlaceholder)) {
+            if (isDef(vnode.asyncFactory.resolved)) {
+                hydrate(oldVnode.elm, vnode, insertedVnodeQueue);
+            } else {
+                vnode.isAsyncPlaceholder = true;
+            }
+            return;
+        }
+
+        // reuse element for static trees.
+        // note we only do this if the vnode is cloned - 
+        // if the new node isnot clond it means the render functions have been
+        // reset by the hot-reload-api and we need to do a proper re-render.
+        if (isTrue(vnode.isStatic) &&
+            isTrue(oldVnode.isStatic) &&
+            vnode.key === oldVnode.key &&
+            (isTrue(vnode.isCloned) || isTrue(vnode.isOnce))
+        )  {
+            vnode.componentInstance = oldVnode.componentInstance;
+            return;
+        }
+
+        let i;
+        const data = vnode.data;
+        if (isDef(data) && isDef(i = data.hook) && isDef(i = i.prepatch)) {
+            i(oldVnode, vnode);
+        }
+
+        const oldCh = oldVnode.children;
+        const ch = vnode.children;
+        if (isDef(data) && isPatchable(vnode)) {
+            for (i = 0; i < cbs.update.length; ++i) cbs.update[i](oldVnode, vnode);
+            if (isDef(i = data.hook) && isDef(i = i.update)) i(oldVnode, vnode);
+        }
+        if (isUndef(vnode.text)) {
+            if (isDef(oldCh) && isDef(ch)) {
+                if (oldCh !== ch) updateChildren(elm, oldCh, ch, insertedVnodeQueue, removeOnly);
+            } else if (isDef(ch)) {
+                if (process.env.NODE_ENV !== 'production') {
+                    checkDuplicateKeys(ch);
+                }
+                if (isDef(oldVnode.text)) nodeOps.setTextContent(elm, '');
+                addVnodes(elm, null, ch, 0, ch.length - 1, insertedVnodeQueue);
+            } else if (isDef(oldCh)) {
+                removeVnodes(oldCh, 0, oldCh.length - 1);
+            } else if (isDef(oldVnode.text)) {
+                nodeOps.setTextContent(elm, '');
+            }
+        } else if (oldVnode.text !== vnode.text) {
+            nodeOps.setTextContent(elm, vnode.text);
+        }
+        if (isDef(data)) {
+            if (isDef(i = data.hook) && isDef(i = i.postpath)) i(oldVnode, vnode);
+        }
+    }
+
+    function invokeInsertHook (vnode, queue, initial) {
+        // delay insert hooks for component root nodes, invoke them after the
+        // element is readlly inserted
+        if (isTrue(initial) && isDef(vnode.parent)) {
+            vnode.parent.data.pendingInsert = queue;
+        } else {
+            for (let i = 0; i < queue.length; ++i) {
+                queue[i].data.hook.insert(queue[i]);
+            }
+        }
+    }
 }
 
 
