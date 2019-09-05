1c379b3cfa7fa37fe643bb6d51469cbe670569ad
hgx <1521550385@qq.com>
2019-09-05 13:02:17 +0800

    'update-compiler/directives/bind.js'

---
+++ vue/vue/src/compiler/directives/bind.js
@@ -0,0 +1,12 @@
+/* flow */
+
+export default function bind(el: ASTElement, dir: ASTDirective) {
+    el.wrapData = (code: string ) => {
+        return `_b(${code},'${e.tag}',${dir.value}),${
+            dir.modifiers && dir.modifiers.prop ? 'true' : 'false'
+        }${
+            dir.modifiers && dir.modifiers.sync ? ',true' : ''
+        }`
+    }
+}
+
