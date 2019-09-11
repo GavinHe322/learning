a5169c5c206dc574d587fdd9d4d0fbcb7a42b130
hgx <1521550385@qq.com>
2019-09-11 13:29:14 +0800

    'update-compiler/codegen/index.js'

--- vue/vue/src/compiler/codegen/index.js
+++ vue/vue/src/compiler/codegen/index.js
@@ -191,4 +191,205 @@ export function genFor (
     altHelper?: string
 ): string {
     const exp = el.for;
+    const alias = el.alias;
+    const iterator1 = el.iterator1 ? `,${el.iterator1}` : '';
+    const iterator2 = el.iterator2 ? `,${el.iterator2}` : '';
+
+    if (process.env.NODE_ENV !== 'production' &&
+        state.maybeComponent(el) &&
+        el.tag !== 'slot' &&
+        el.tag !== 'template' &&
+        !el.key
+    ) {
+        state.warn(
+            `<${el.tag} v-for="${alias} in ${exp}">: component lists rendered with ` +
+            `v-for should have explicit keys. ` +
+            `See https://vue.jsxxxxxxx for more info.`,
+            el.rawAttrsMap['v-for'],
+            true /* tip */
+        );
+    }
+
+    el.forProcessed = true; // avoid recusion
+    return `${altHelper || '_l'}((${exp}),` +
+        `function(${alias}${iterator1}${iterator2}){` +
+            `return ${(altGen || genElement)(el, state)}` +
+        '})'
 }
+
+export function getData (el: ASTElement, state: CodegenState): string {
+    let data = '{';
+
+    // directives first.
+    // directives may mutate the el's other properties before they are generated.
+    const dirs = genDirectives(el, state);
+    if (dirs) data += dirs + ',';
+
+    // key
+    if (el.key) {
+        data += `key:${el.key},`
+    }
+    // ref
+    if (el.ref) {
+        data += `ref:${el.ref}`;
+    }
+    if (el.refInFor) {
+        data += `refInFor:true`;
+    }
+
+    // pre
+    if (el.pre) {
+        data += `pre:true,`
+    }
+    
+    // record original tag name for components using "is" attribute
+    if (el.component) {
+        data += `tag:"${el.tag}",`;
+    }
+    // module data generation functions
+    for (let i = 0; i < state.dataGenFns.length; i++) {
+        data += state.dataGenFns[i](el);
+    }
+
+    // attributes
+    if (el.attrs) {
+        data += `attrs:${genProps(el.attrs)},`
+    }
+
+    // Dom props
+    if (el.props) {
+        data += `attrs:${genProps(el.props)},`;
+    }
+    // event handlers
+    if (el.events) {
+        data += `${genHandlers(el.events, false)},`;
+    }
+    if (el.nativeEvents) {
+        data += `${genHandlers(el.nativeEvents, true)}`;
+    }
+
+    // slot target
+    // only for non-scoped slots
+    if (el.slotTarget && !el.slotScope) {
+        data += `slot:${el.slotTarget},`
+    }
+
+    // component v-model
+    if (el.model) {
+        data += `model:{value:${
+            el.model.value
+        },callback:${
+            el.model.callback
+        },expression:${
+            el.model.expression
+        }},`;
+    }
+    // inline-template
+    if (el.inlineTemplate) {
+        const inlineTemplate = genInlineTemplate(el, state);
+        if (inlineTemplate) {
+            data += `${inlineTemplate},`;
+        }
+    }
+    data = data.replace(/,$/, '') + '}';
+    // v-bind dnamic argument wrap
+    // v-bind with dynamic arguments must be applied using the same v-bind object
+    // merge helper so that class/style/mustUseProp are handled correctly.
+    if (el.dynamicAttrs) {
+        data = `_b(${data},"${el.tag}",${genProps(el.dynamicAttrs)})`;
+    }
+    // v-bind data wrap
+    if (el.wrapData) {
+        data = el.wrapData(data);
+    }
+    // v-on data wrap
+    if (el.wrapListeners) {
+        data = el.wrapListeners(data);
+    }
+    return data;
+}
+
+function genDirectives (el: ASTElement, state: CodegenState): string | void {
+    const dirs = el.directives;
+    if (!dirs) return;
+    let res = 'directives:[';
+    let hasRuntime = false;
+    let i, l, dir, needRuntime;
+    for (i = 0, l = dirs.length; i < l; i++) {
+        dir = dirs[i];
+        needRuntime = true;
+        const gen: DirectiveFunction = state.directives[dir.name];
+        if (gen) {
+            // compile-time directive that manipulates AST.
+            // returns true if it also needs a runtime counterpart.
+            needRuntime =  !!gen(el, dir, state.warn);
+        }
+        if (needRuntime) {
+            hasRuntime = true;
+            res += `{name:"${dir.name}", rawname:"${dir.rawName}"${
+                dir.value ? `,value:(${dir.value}),expression:${JSON.stringify(dir.value)}` : ''
+            }${
+                dir.arg ? `,arg:${dir.isDynamicArg ? dir.arg : `"${dir.arg}"`}` : ''
+            }${
+                dir.modifiers ? `,modifiers:${JSON.stringify(dir.modifiers)}` : ''
+            }}`;
+        }
+    }
+    if (hasRuntime) {
+        return res.slice(0, -1) + ']';
+    }
+}
+
+function genInlineTemplate (el: ASTElement, state: CodegenState): ?string {
+    const ast = el.children[0];
+    if (process.env.NODE_ENV !== 'production' && (
+        el.children.length !== 1 || ast.type !== 1
+    )) {
+        state.warn(
+            'Inline-template components must have exactly one child element.',
+            { start: el.start}
+        );
+    }
+    if (ast && ast.type === 1) {
+        const inlineRenderFns = generate(ast, state.options);
+        return `inlineTemplate:{render:function(){${
+            inlineRenderFns.render
+        }},staticRenderFns:[${
+            inlineRenderFns.staticRenderFns.map(code => `function(){${code}}`).join(',')
+        }]}`
+    }
+}
+
+function genScopedSlots (
+    el: ASTElement,
+    slots: { [key: string]: ASTElement },
+    state: CodegenState
+): string {
+    // by default scoped slots are considered "stable", this allows child
+    // components with only csoped slots to skip forced updates from parent.
+    // but in some cases we have to bail-out this optimization
+    // for example if the slot cantains dynamic names, has v-if or v-for on them...
+    let needsForceUpdate = el.for || Object.keys(slots).some(key => {
+        const slot = slots[key];
+        return (
+            slot.slotTargetDynamic || 
+            slot.if || 
+            slot.for ||
+            containsSlotChild(slot)  // is passing down slot from parent which may be dynamic
+        );
+    })
+
+    // #9534: if a componet with scoped slots is inside a conditional branch,
+    // it's possible for the same component to be reused but with different
+    // compiled slot comtent. To avoid that, we generate a unique key based on
+    // the generated code of all the slot contents.
+    let needsKey = !!el.if;
+
+    // OR when it is inside another scoped slot or v-for (the reactivity may be
+    // disconnected due to the intermediate scope variable)
+    // #9438, #956
+    // TODO: this can be further optimized by properly analyzing in-scope bindings
+    // and skip force updating ones that do not actually use scope variables.
+}
+
+
