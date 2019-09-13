e80ed31db58b0543cd59657bef369ac5f7f099fc
GavinHe322 <1521550385@qq.com>
2019-09-13 22:28:19 +0800

    update-shell-test.sh-variable.sh

---
+++ shell/variable.sh
@@ -0,0 +1,61 @@
+#!/bin/bash
+
+#-----------------------
+# your_name="tom";
+# echo $your_name;
+
+# your_name="alibaba";
+# echo $your_name;
+
+# for shill in Ada Coffe Action Java; do
+#     echo "I am good at ${skill}Script"
+# done
+
+# your_name='runoob'
+# str="Hello, I know you are \"$your_name\"! \n"
+# echo -e $str
+#-------------------------
+
+
+#---------------------------------
+# your_name="runoob"
+# # 使用双引号拼接
+# greeting="Hello,"$your_name" !"
+# greeting_1="hello,${your_name} !"
+# echo $greeting $greeting_1
+
+# # 使用单引号拼接
+# greeting_2='hello, '$your_name' !'
+# greeting_3='hello, ${your_name} !'
+# echo $greeting_2 $greeting_3
+
+# string='abcd'
+# echo ${#string} #输出4
+
+# string="runoob is a great site"
+# echo `expr index "$string" io` # 输出4
+
+# array_name=(1, 2, 3, 3)
+
+# # 取得数组元素的个数
+# length=${#array_name[@]}
+# echo $length
+# # 或者
+# length=${#array_name[*]}
+# echo $length
+# # 取得数组单个元素的长度
+# lengthn=${#array_name[n]}
+# echo $length
+#------------------------------
+
+#--------------------------
+# echo "-- \$* print ---"
+# for i in "$*"; do
+#     echo $i
+# done
+
+# echo "-- \$@ print ---"
+# for i in "$@"; do
+#     echo $i
+# done
+#--------------------------
