bd563e9555c1da828f526a82b8ffca521ae519d8
GavinHe322 <https://github.com/GavinHe322>
2019-09-14 00:17:59 +0800

    什么gui

--- commit.sh
+++ commit.sh
@@ -2,10 +2,11 @@
 # author: Gavin
 # description: 'git commit script'
 
-echo ""$0""
+echo "$0"
 echo "$1"
+# echo "$1"
 echo '-------------------------------'
 git add -A
-git commit -m ""$1""
+git commit -m "$1"
 git push origin
 
