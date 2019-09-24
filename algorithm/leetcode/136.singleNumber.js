f9cfa5c8c7229e3b412137a5b1651f6d7a0728aa
hgx <1521550385@qq.com>
2019-09-24 14:13:44 +0800

    'update-just_show_one_num'

--- algorithm/leetcode/136.singleNumber.js
+++ algorithm/leetcode/136.singleNumber.js
@@ -2,13 +2,24 @@
  * @param {number[]} nums
  * @return {number}
  */
+
 var singleNumber = function(nums) {
-    var ans = 0;
-    for (const num of nums) {
-        ans ^= num;
+    // for (let i = 0; i < nums.length; i++) {
+    //     if (nums.lastIndexOf(nums[i]) === nums.indexOf(nums[i])) return nums[i]
+    // }
+    // return ans;
+    let numsObj = {}
+
+    for (let i = 0, l = nums.length; i < l; i++) {
+        if (numsObj[nums[i]]) {
+            delete numsObj[nums[i]]
+        } else {
+            numsObj[nums[i]] = 1
+        }
     }
-    return ans;
+    return Object.keys(numsObj)[0]
 };
+
 console.log(
-    singleNumber([1,2,3,4])
+    singleNumber([1, 1, 2, 3, 4, 4])
 )
