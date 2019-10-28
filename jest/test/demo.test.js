5344c253ae129995a083e299f69072943aac6790
GavinHe322 <https://github.com/GavinHe322>
2019-10-29 00:03:38 +0800

    update-jest-通配符demo.api阅读完毕

--- jest/test/demo.test.js
+++ jest/test/demo.test.js
@@ -69,15 +69,29 @@
 
 
 // 5. 数组和可迭代器
-const shoppingList = [
-    'diapers',
-    'kleenex',
-    'trash bags',
-    'paper towels',
-    'beer'
-]
+// const shoppingList = [
+//     'diapers',
+//     'kleenex',
+//     'trash bags',
+//     'paper towels',
+//     'beer'
+// ]
 
-test('the shopping list has beer on it', () => {
-    expect(shoppingList).toContain('beer')
-    expect(new Set(shoppingList)).toContain('beer')
-})
+// test('the shopping list has beer on it', () => {
+//     expect(shoppingList).toContain('beer')
+//     expect(new Set(shoppingList)).toContain('beer')
+// })
+
+// 例外情况
+
+function compileAndroidCode() {
+    throw new Error('you are using the wrong JDK');
+}
+  
+test('compiling android goes as expected', () => {
+    expect(compileAndroidCode).toThrow();
+    expect(compileAndroidCode).toThrow(Error);
+  
+    expect(compileAndroidCode).toThrow('you are using the wrong JDK');
+    expect(compileAndroidCode).toThrow(/JDK/);
+});
