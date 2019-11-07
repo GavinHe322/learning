5145049a0f85c953869f6699b17db780cf6bf244
GavinHe322 <https://github.com/GavinHe322>
2019-11-07 22:41:07 +0800

    feat: dart 基础类型

---
+++ dart/1.hello.dart
@@ -0,0 +1,17 @@
+
+// main 标准写法
+// void main() {
+//   print('Hello world'); // 注意 Dart 和 Java 必须以分号结尾.
+// }
+
+// dart 中 void 类型，
+// main() {
+//   print('Hello world');
+// }
+
+
+// 支持箭头函数
+// void main() => print('Hello world');
+
+// 省略 void
+main() => print('Hello world');
--- dart/test.dart
+++
@@ -1,9 +0,0 @@
-
-
-
-main(List<String> args) {
-  var name = "hi"; //String类型
-  var age = '18'; //int类型
-  var high = '1.70'; //double类型
-  print(name + '  '  +  age + '' + high);
-}
