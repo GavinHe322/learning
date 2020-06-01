fa4c754e92a0d7dc7c2d69751154c6244b6949cf
GavinHe322 <https://github.com/GavinHe322>
2020-06-01 23:13:15 +0800

    feat: hello java

---
+++ java/base/01_Hello/Hello.java
@@ -0,0 +1,8 @@
+//#java
+// export JAVA_HOME=/opt/jdk-14.0.1.jdk/Contents/Home
+// export PATH=$JAVA_HOME/bin:$PATH
+public class Hello {
+    public static void main(String[] args) {
+        System.out.println("Hello, world!");
+    }
+}
