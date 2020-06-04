2422f0ed289a1d88765865a949d2a29c1fa722af
GavinHe322 <https://github.com/GavinHe322>
2020-06-04 23:24:02 +0800

    feat: java module 有点复杂

---
+++ java/base/05_object/01_base/oop-module/src/com/itranswarp/sample/Main.java
@@ -0,0 +1,10 @@
+package com.itranswarp.sample;
+
+// 必须引入java.xml模块后才能使用其中的类:
+import javax.xml.XMLConstants;
+
+public class Main {
+	public static void main(String[] args) {
+		System.out.println(XMLConstants.XML_NS_PREFIX);
+	}
+}
