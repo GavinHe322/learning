f0d3c55a6f5d9c128c5705adda5e09ac396e033f
GavinHe322 <https://github.com/GavinHe322>
2020-08-03 21:28:03 +0800

    feat: ts @ts-check

---
+++ ts/23_check.ts
@@ -0,0 +1,27 @@
+// @ts-check
+
+// /**
+//  * 支持的 jsDoc
+//  * @type
+//  * @param (or @arg or @argument)
+//  * @returns (or @return)
+//  * @typedef
+//  * @callback
+//  * @template
+//  * @class (or @constructor)
+//  * @this
+//  * @extends (or @augments)
+//  * @enum
+//  */
+
+/**
+ * @type {string}
+ */
+var s
+s = '2'
+s = 2
+
+/** @type {Window} */
+var win
+win = window
+win = 20
