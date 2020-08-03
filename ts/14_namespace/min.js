975ccd4d027905e4daedf2b5589e566801506458
GavinHe322 <1521550385@qq.com>
2020-08-03 13:26:54 +0800

    delete: ts compiled's js

--- ts/14_namespace/min.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-exports.__esModule = true;
-var MinClass_1 = require("./MinClass");
-console.log(MinClass_1.A.MinClass);
-var min = new MinClass_1.A.MinClass();
-min.add(12);
-min.add(11);
-min.add(20);
-console.log(min.min());
