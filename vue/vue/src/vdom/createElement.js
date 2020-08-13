7d2f20721d79d84b938ec852ed2f47341c070e30
GavinHe322 <1521550385@qq.com>
2020-08-13 15:43:27 +0800

    fix: _createElement context参数

--- vue/vue/src/vdom/createElement.js
+++ vue/vue/src/vdom/createElement.js
@@ -3,6 +3,6 @@ function createElement(context, tag, data, children) {
 }
 
 function _createElement(context, tag, data, children) {
-  let vnode = new VNode(tag, data, children)
+  let vnode = new VNode(tag, data, children, undefined, undefined, context)
   return vnode
 }
